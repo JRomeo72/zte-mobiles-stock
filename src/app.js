f051769a9ce0f363ab351be1d2bd2d1bf6a6da91
Romeo Salomón <jromeo.hs@gmail.com>
2024-02-10 17:04:35 -0600

    Se modifico el PORT

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ env.config()
 
 dbConnect()
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 4003;
 
 app.listen(PORT, () => {
     console.log(`Server run on port ${PORT}`);
